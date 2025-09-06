dcf8421dacf314c7eba6cae37819035678be6a2f
pawansinha0612 <pawansinha4u@gmail.com>
2025-09-06 20:22:11 +1000

    Update backend URL to Render for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,7 @@
 export const fetchOffers = async () => {
     try {
-        const res = await fetch("http://127.0.0.1:5000/offers");
+
+        const res = await fetch("https://offers-hub.onrender.com/offers")
         return await res.json();
     } catch (err) {
         console.error("Failed to fetch offers:", err);
